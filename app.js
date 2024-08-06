617534ca00107af8eb5760b3cf18b704d20c4a0f
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2024-08-06 23:41:32 +0500

    fetch function code complete

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 //js start
+//IIFE immediatly invoked function expression
+//fetch function
+( async function(){
+const response = await fetch("./recipes.json");
+const recipes = await  response.json()
+console.log(recipes)
+})();
